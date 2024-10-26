444748982e7c13162c37b5facb85a75f953ee1fc
Akash Kumar <34508656+krrakash@users.noreply.github.com>
2024-10-26 17:07:09 +0530

    Update MultiSigWallet.sol
    imports corrected

--- MultiSigWallet.sol
+++ MultiSigWallet.sol
@@ -5,6 +5,7 @@ import "@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol";
 
 interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
 }
 
 interface IERC721 {
